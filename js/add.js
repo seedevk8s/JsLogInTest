de085f81e6ee4cb7e20fa0f2803a9e56c91e31cd
D191375 <seedevk8s@gmail.com>
2020-01-30 16:26:28 +0900

    add.js의 main()함수 -- 버튼에 이벤트 연결

--- js/add.js
+++ js/add.js
@@ -2,6 +2,33 @@ document.addEventListener('DOMContentLoaded', main);
 
 function main() {
     // 버튼에 이벤트 연결
+    bindSaveButton();
+
     // 토큰 체크
     // 토큰으로 서버에서 나의 정보 받아오기
 }
+
+function bindSaveButton() {
+    const form = document.querySelector('#form-add-book');
+    form.addEventListener('submit', save);
+}
+
+function save() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
