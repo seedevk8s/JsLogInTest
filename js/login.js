e18c8b23f53a4177c774216be6c222782ceb9cb9
D191375 <seedevk8s@gmail.com>
2020-01-30 13:55:21 +0900

    login 함수까지 연결

--- js/login.js
+++ js/login.js
@@ -2,7 +2,18 @@ document.addEventListener('DOMContentLoaded', main);
 
 function main() {
     // 버튼에 이벤트 연결
+    bindLoginButton();
 
     // 토큰 체크
 
 }
+
+function bindLoginButton() {
+    const form = document.querySelector('#form-login');
+    form.addEventListener('submit', login);
+}
+
+function login() {
+    
+}
+
